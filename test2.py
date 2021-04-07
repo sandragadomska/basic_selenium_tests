38b6447c0e23dbe728fee5c9ae7f298f3c2e510b
Sandra Gadomska <gadomska.sandra@gmail.com>
2021-04-07 17:54:50 +0200

    initial commit

---
+++ test2.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+PATH = "C:\Program Files\chromedriver.exe"
+
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://bandcamp.com")
+
+driver.find_element_by_class_name("play-btn").click()
